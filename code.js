c6d431222c0f4831b970885a460f9251c6329f4e
BambooCode98 <jw.thomas98@yahoo.com>
2021-11-10 10:12:04 -0600

    The beginning template for the grid

---
+++ code.js
@@ -0,0 +1,16 @@
+const gridContainerBox = document.getElementsByClassName('gridcontainer');  //remember this is an array-like thing
+const gridSquare = document.createElement('div');
+gridSquare.style.border = "1px red solid";
+gridContainerBox[0].appendChild(gridSquare);
+
+function createChildElements() {    // a function like this can be presented to user to select a number of sqaures
+
+}
+
+
+
+
+
+
+
+
