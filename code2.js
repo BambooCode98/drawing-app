1dc1958de801fb25043dd8fa10593a433ae078db
BambooCode98 <jw.thomas98@yahoo.com>
2022-07-09 23:04:16 -0500

    added ability to download drawing

--- code2.js
+++ code2.js
@@ -9,13 +9,16 @@ let blue = document.querySelector('.blue');
 let orange = document.querySelector('.orange');
 let black = document.querySelector('.black');
 let rby = document.querySelector('.rby');
+let eraserSq = document.querySelector('.eraserSquare');
+let eraserC = document.querySelector('.eraserCircle');
+let download = document.querySelector('.download');
 let coords = {x:0, y:0};
 let touchCoords = {x:0, y:0};
-// let {x,y} = {0,0};
 let hue = 0;
 let drawing = false;
 let colors = "";
 let colorInt;
+let lineEnd = "";
 
 // ctx.canvas.width = window.innerWidth;
 // ctx.canvas.height = window.innerHeight;
@@ -42,7 +45,25 @@ yellow.addEventListener('click', colorChanger)
 orange.addEventListener('click', colorChanger)
 red.addEventListener('click', colorChanger)
 black.addEventListener('click', colorChanger)
+eraserSq.addEventListener('click', colorChanger)
+eraserC.addEventListener('click', colorChanger)
 rby.addEventListener('click', multiColor)
+
+download.addEventListener('click', (e) => {
+  let canvasURL = canvas.toDataURL();
+  const createAnchor = document.createElement('a');
+  createAnchor.href = canvasURL;
+  createAnchor.download = "yourDrawing.png";
+  createAnchor.click();
+  createAnchor.remove();
+})
+
+// async function downloadCanvas() {
+//   let link = document.querySelector('.link');
+//   link.setAttribute('downlaod', 'yourDrawing.png');
+//   link.setAttribute('href', canvas.toDataURL('yourDrawing.png'));
+//   link.click();
+// }
 
 function initial(event) {
   mousePosition(event);
@@ -53,10 +74,12 @@ function draw(event) {
   if (drawing == false) return;
   ctx.beginPath();
   ctx.lineWidth = size.value;
-  ctx.lineCap = "round";
+  if(lineEnd === "square") {
+    ctx.lineCap = "square";
+  } else {
+    ctx.lineCap = "round";
+  }
   ctx.lineJoin = "round";
-  // hue++;
-  // multiColor();
   ctx.strokeStyle = colors;
   ctx.moveTo(coords.x,coords.y);
   mousePosition(event)
@@ -74,7 +97,7 @@ function stop() {
     drawing = false;
     coords.x = 0;
     coords.y = 0;
-    clearInterval(colorInt);
+    lineEnd = "round";
   }
 }
 
@@ -83,11 +106,7 @@ function initialTouch(e) {
   [...e.changedTouches].forEach(touch => {
     touchPosition(touch);
     console.log(touchCoords);
-    ctx.beginPath();
-    ctx.lineWidth = size.value;
-    ctx.lineCap = "round";
-    ctx.lineJoin = "round";
-    ctx.strokeStyle = colors;
+    
     
   })
   drawing = true;
@@ -96,6 +115,15 @@ function initialTouch(e) {
 function drawTouch(e) {
   if (drawing === false) return;
   [...e.changedTouches].forEach( touch => {
+    ctx.beginPath();
+    ctx.lineWidth = size.value;
+    if(lineEnd === "square") {
+      ctx.lineCap = "square";
+    } else {
+      ctx.lineCap = "round";
+    }
+    ctx.lineJoin = "round";
+    ctx.strokeStyle = colors;
     ctx.moveTo(touch.pageX,touch.pageY);
     touchPosition(touch);
     ctx.lineTo(touchCoords.x,touchCoords.y);
@@ -119,6 +147,7 @@ function stopTouch(e) {
     drawing = false;
     touchCoords.x = 0;
     touchCoords.y = 0;
+    lineEnd = "round";
     // [...e.changedTouches].forEach( touch => {
       
     // })
@@ -131,41 +160,55 @@ function sizeSetting() {
 
 function clearCanvas() {
   ctx.clearRect(0,0,canvas.width,canvas.height)
+  clearInterval(colorInt);
+  colors = "black";
 }
 
 function colorChanger(e) {
   // console.log(e.composedPath()[0].classList[0]);
   if(e.composedPath()[0].classList[0] === "green") {
+    clearInterval(colorInt);
     colors = "green";
     return colors;
   } else if(e.composedPath()[0].classList[0] === "red") {
+    clearInterval(colorInt);
     colors = "red";
     return colors;
   } else if(e.composedPath()[0].classList[0] === "orange") {
+    clearInterval(colorInt);
     colors = "orange";
     return colors;
   } else if(e.composedPath()[0].classList[0] === "yellow") {
+    clearInterval(colorInt);
     colors = "yellow";
     return colors;
   } else if(e.composedPath()[0].classList[0] === "blue") {
+    clearInterval(colorInt);
     colors = "blue";
     return colors;
   } else if(e.composedPath()[0].classList[0] === "black") {
+    clearInterval(colorInt);
     colors = "black";
     return colors;
-  } else if(e.composedPath()[0].classList[0] === "rby") {
-    // colorInt = setInterval(multiColor(), 10)
-    // multiColor();
+  } else if(e.composedPath()[0].classList[0] === "eraserSquare") {
+    clearInterval(colorInt);
+    colors = "white";
+    lineEnd = 'square';
     return colors;
-  }
+  }  else if(e.composedPath()[0].classList[0] === "eraserCircle") {
+    clearInterval(colorInt);
+    colors = "white";
+    lineEnd = 'circle';
+    return colors;
+  } 
 }
 
 function multiColor() {
-  // hue++;
   colorInt = setInterval( () => {
     hue++;
+    // console.log(hue);
+    colors = `hsl(${hue},100%,50%)`;
+    return colors;
   }, 10)
-  colors = `hsl(${hue},100%,50%)`;
   console.log(colors);
-  return colors;
 }
